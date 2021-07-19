9953f2d2fe0add29ec39b4e29ab024c9268d638f
Ayush024 <ayush@stepsetgo.com>
2021-07-20 01:29:29 +0530

    created index.js file for move data from csv to MongoDB

--- config/index.js
+++
@@ -1,5 +0,0 @@
-let config = {
-    port: 3001
-};
-
-module.exports = config;
--- index.js
+++ index.js
@@ -1,19 +1,103 @@
-const http = require('http');
-const server = http.createServer();
-const controller = require('./controller/service');
-const db = require('./models/db');
+const mongo = require('mongodb').MongoClient;
+const fs = require('fs');
+const readline = require('readline');
 const yargs = require('yargs');
-const config = require('./config/index');
+const arguments = yargs.argv;
 
-var arguments = yargs.argv;
+const printHelp = () => {
+    console.log(`
+Follow the following given parameter to use this tool -
+    --dbtype            - Provides database type [mongodb, postgres]
+    --database          - Name of the database
+    --collection        - Name of the collection or the table
+    --filepath          - Location of the file to be parsed
+    --columns           - comma separated values of the columns
+    `);
+};
 
-db.passData(arguments);
+const printColumnHelp = () => {
+    console.log(`
+Please provide _id in column inorder to prevent Key Duplication Error
+    `)
+}
 
-controller.addDetailstoDb(arguments);
+const parseFile = async () => {
 
-server.listen(config.port, () => {
+    const FILE_PATH = arguments.filepath;
 
-    console.log(`App listening at http://localhost:${config.port}`)
+    const DB_TYPE = arguments.dbtype;
 
-});
+    const DB_NAME = arguments.database;
 
+    const COLLECTION_NAME = arguments.collection;
+
+    const COLUMNS = arguments.columns.split(",");
+
+    if (!FILE_PATH || !DB_TYPE || !DB_NAME || !COLLECTION_NAME) {
+        console.error('No correct value provided');
+        printHelp();
+        return;
+    }
+
+    if (!COLUMNS.includes("_id")){
+        console.error('No correct value provided');
+        printColumnHelp();
+        return;
+    }
+
+    try {
+
+        client = new mongo(`${DB_TYPE}://localhost:27017`);
+
+        client.connect();
+
+        const database = client.db(DB_NAME);
+
+        const collection = database.collection(COLLECTION_NAME);
+
+        const rl = readline.createInterface({
+            input: fs.createReadStream(FILE_PATH),
+            crlfDelay: Infinity
+        });
+
+        let user = {};
+
+        let headers;
+
+        let rowCount = 0;
+        
+        let needed_columns = [];
+
+        for await (const line of rl) {
+            if (rowCount == 0) {
+                headers = line.split(',');
+
+                for (let j = 0; j < headers.length; j++) {
+
+                    if (COLUMNS.includes(headers[j])) {
+                        needed_columns.push(j)
+                    }
+                }
+                rowCount += 1;
+            } else {
+
+                rows = line.split(',');
+
+                for (let j = 0; j < needed_columns.length; j++) {
+                    var name = headers[needed_columns[j]]
+                    user[name] = rows[j]
+                }
+
+                const result = await collection.insertOne(user);
+            }
+
+        }
+
+    } catch (err) {
+        console.log(err);
+    }
+
+};
+
+
+parseFile();
